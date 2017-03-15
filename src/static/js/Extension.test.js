0726be393998305ece8ef498b667986396d7d8ba
Ekaterina Loshchinina <KateLoschinina@Ekaterinas-MacBook-Pro-3.local>
2017-03-15 18:10:49 +0000

    Can test Chrome Extensiongit add .git add .git add .git add . We Made Itgit add .git add .

--- src/static/js/Extension.test.js
+++ src/static/js/Extension.test.js
@@ -2,6 +2,7 @@ jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
 
 it('Extention add text to the google search results',  (done)=> {
   var sw = require('selenium-webdriver');
+  var assert = require('chai').assert;
   var chromeOptions = sw.Capabilities.chrome();
   chromeOptions.set("chromeOptions",  {"args": ['--load-extension='+"/Users/KateLoschinina/Workspace/MakersAcademy/devlinq"]});
   // console.log(chromeOptions)
@@ -12,55 +13,21 @@ it('Extention add text to the google search results',  (done)=> {
 
   driver.get('https://www.google.co.uk/#q=search&*')
     .then(function(){
-      console.log(1)
-      // done();
-    })
-    .then(function(){
-      console.log(2)
-      // driver.findElement(sw.By.id('main')).then(function(el){
-      //   console.log(el)
-      //   el.click();
-      // });
-      // var a = driver.findElement(sw.By.id('appbar'));
-      // a.click();
-      // console.log(a)
       driver.findElement(sw.By.id('searchform')).then(function(a) {
         setTimeout(function() {
-          console.log(a);
           a.click().then(function(b) {
-            console.log(b)
-            
+            driver.findElement(sw.By.id('stack')).then(function(c){
+              c.getText().then(function(d){
+                assert.equal(d,'Hi there and greetings!');
+                done();
+              })
+              done();
+            })
             done();
           }).catch(function(c) {
-            console.log(c)
             done();
           })
         }, 5000)
-
-
       })
-      // driver.getPageSource().then(function(a) {
-      //
-      //
-      // });
-      //
-      // a.then(function() {
-      //   console.log(3);
-      //   done();
-      // }).catch(function() {
-      //   console.log("ERROR", 3, arguments);
-      //   done();
-      // })
-
     })
-    // .then(function(q) {
-    //   console.log(3)
-    //   q.click();
-    //
-    // });
-
-    // console.log(r);
-
-  // console.log(driver.findElement({id: 'appbar'}).click());
-
 })
