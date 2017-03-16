e81646d8a80e3f2a52196a0c0aa8578ac290022a
Ekaterina Loshchinina <KateLoschinina@Ekaterinas-MacBook-Pro-3.local>
2017-03-16 14:14:00 +0000

    Checking out from master.

--- src/static/js/languagebar/EventLanguageBar.js
+++ src/static/js/languagebar/EventLanguageBar.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
   setTimeout(function(){
-  // window.onload = function(){
+  window.onload = function(){
     $("#submitSearchButton").click(function() {
+      console.log(1)
       var chosenLanguage = $("#languageDropdownList option:selected").text();
       var chosenVersion = $("#versionDropdownList option:selected").text();
       var chosenTopic = $("#topicDropdownList option:selected").text();
@@ -11,18 +12,20 @@ $(document).ready(function(){
     });
 
     $("#languageDropdownList").change(function(){
-    var chosenLanguage = $("#languageDropdownList option:selected").text();
-    var versionDropdown = document.getElementById("versionDropdownList")
-    $('#versionDropdownList').empty();
-    var list = new LanguagesView().generateVersionOptions(versionDropdown, chosenLanguage);
+      console.log(2)
+      var chosenLanguage = $("#languageDropdownList option:selected").text();
+      var versionDropdown = document.getElementById("versionDropdownList")
+      $('#versionDropdownList').empty();
+      var list = new LanguagesView().generateVersionOptions(versionDropdown, chosenLanguage);
     });
 
     $("#languageDropdownList").change(function(){
+      console.log(3)
       var chosenLanguage = $("#languageDropdownList option:selected").text();
       var topicDropdown = document.getElementById("topicDropdownList")
       $('#topicDropdownList').empty();
       var list = new LanguagesView().generateTopicOptions(topicDropdown, chosenLanguage);
     });
-  // };
+  };
   }, 2000);
 });
