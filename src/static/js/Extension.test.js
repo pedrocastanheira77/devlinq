e6e2d0e73bf049a279a5ec865562061002b2ca3b
Ekaterina Loshchinina <KateLoschinina@Ekaterinas-MacBook-Pro-3.local>
2017-03-16 13:18:18 +0000

    Solve merge issues.

--- src/static/js/Extension.test.js
+++ src/static/js/Extension.test.js
@@ -4,7 +4,7 @@ it('Extention add text to the google search results',  (done)=> {
   var sw = require('selenium-webdriver');
   var assert = require('chai').assert;
   var chromeOptions = sw.Capabilities.chrome();
-  chromeOptions.set("chromeOptions",  {"args": ['--load-extension='+"/Users/Lauren/makers/devlinq"]});
+  chromeOptions.set("chromeOptions",  {"args": ['--load-extension='+"/Users/KateLoschinina/Workspace/MakersAcademy/devlinq"]});
   // console.log(chromeOptions)
   var driver = new sw.Builder()
       .forBrowser('chrome')
