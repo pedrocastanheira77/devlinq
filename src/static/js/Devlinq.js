f4e1ba5b59ec9362d5b0882f629531addd9ab977
frankie <franklin.shaw@gmail.com>
2017-03-17 17:53:51 +0000

    add stackoverflow div

--- src/static/js/Devlinq.js
+++ src/static/js/Devlinq.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
   var optionsDiv = new LanguagesView().createLanguageDiv();
   var officialDiv = new OfficialDocsView().createOfficialDiv();
+  var stackOverflowDiv = new StackOverflowBar().createStackOverflowDiv();
+  stackOverflowDiv.id = "stackoverflowbar";
+
   setTimeout(function() {
     var currentDiv = document.getElementById("appbar");
     currentDiv.parentNode.insertBefore(optionsDiv, currentDiv);
     currentDiv.parentNode.insertBefore(officialDiv, currentDiv);
-
-    var stackOverflowDiv = document.createElement("div");
-    stackOverflowDiv.id = "stackoverflowbar";
     currentDiv.parentNode.insertBefore(stackOverflowDiv, currentDiv);
   }, 5000);
 });
