3a44e735cd67b32d0b84a2c6876feba3f4d8b876
Ekaterina Loshchinina <KateLoschinina@ekaterinasmbp2.home>
2017-03-19 21:39:29 +0000

    Refactor main application file Devlinq.js

--- src/static/js/Devlinq.js
+++ src/static/js/Devlinq.js
@@ -1,30 +1,61 @@
-var StackOverflowBar = require("./stackoverflowbar/StackOverflowBar.js");
+
 var LanguagesView = require("./languagebar/LanguagesBar.js");
+var lang = new LanguagesView();
 var createOfficialDiv = require("./languagebar/OfficialDocsResults.js").createOfficialDiv;
+var StackOverflowBar = require("./stackoverflowbar/StackOverflowBar.js");
+var stackbar = new StackOverflowBar();
 
-// document.addEventListener('DOMContentLoaded', function() {
-  var lang = new LanguagesView();
-  var optionsDiv = lang.createLanguageDiv();
-  var officialDiv = createOfficialDiv();
+devlinqExtention()
+
+function devlinqExtention() {
   setTimeout(function() {
-    var spinnerDiv = document.querySelector("#spinner");
-    if (spinnerDiv) {
-      spinnerDiv.parentNode.removeChild(spinnerDiv);
-    }
+    createSpinner();
     var currentDiv = document.getElementById("appbar");
-    currentDiv.parentNode.insertBefore(optionsDiv, currentDiv);
-    currentDiv.parentNode.insertBefore(officialDiv, currentDiv);
-
-    var stackOverflowDiv = document.createElement("p");
-    stackOverflowDiv.id = "stackoverflowbar";
-    currentDiv.parentNode.insertBefore(stackOverflowDiv, currentDiv);
-    var stackbar = new StackOverflowBar();
-    var numberOfLinks = 5;
-    var stackoverflowsearch = stackbar.decideStringForAPI()
-    stackbar.getStackAPI(stackoverflowsearch, numberOfLinks).then(function(items){
-      for(var i = 0; i < numberOfLinks; i++){
-        stackOverflowDiv.insertAdjacentHTML('beforeend', '<p><b>'+items[i].getTitle()+'</b>\n'+items[i].getUrl()+'</p>');
-      }
-    })
+    languagesDiv(currentDiv);
+    stackOverflowDiv(currentDiv);
   }, 3000);
-// });
+}
+
+function createSpinner() {
+  var spinnerDiv = document.querySelector("#spinner");
+  if (spinnerDiv) {
+    spinnerDiv.parentNode.removeChild(spinnerDiv);
+  };
+}
+
+function languagesDiv(currentDiv) {
+  insertDropdownIntoLanguages(currentDiv);
+  insertOfficialDocsIntoLanguages(currentDiv);
+}
+
+function insertDropdownIntoLanguages(currentDiv) {
+  var optionsDiv = lang.createLanguageDiv();
+  currentDiv.parentNode.insertBefore(optionsDiv, currentDiv);
+}
+
+function insertOfficialDocsIntoLanguages(currentDiv) {
+  var officialDiv = createOfficialDiv();
+  currentDiv.parentNode.insertBefore(officialDiv, currentDiv);
+}
+
+function stackOverflowDiv(currentDiv) {
+  var stackOverflowDiv = createStackOverflowDiv();
+  currentDiv.parentNode.insertBefore(stackOverflowDiv, currentDiv);
+  var numberOfLinks = 5;
+  insertStackOverflowAPI(numberOfLinks, stackOverflowDiv);
+}
+
+function createStackOverflowDiv() {
+  var stackOverflowDiv = document.createElement("p");
+  stackOverflowDiv.id = "stackoverflowbar";
+  return stackOverflowDiv;
+}
+
+function insertStackOverflowAPI(numberOfLinks, stackOverflowDiv){
+  var stackoverflowsearch = stackbar.decideStringForAPI();
+  stackbar.getStackAPI(stackoverflowsearch, numberOfLinks).then(function(items){
+    for(var i = 0; i < numberOfLinks; i++){
+      stackOverflowDiv.insertAdjacentHTML('beforeend', '<p><b>'+items[i].getTitle()+'</b>\n'+items[i].getUrl()+'</p>');
+    }
+  });
+}
