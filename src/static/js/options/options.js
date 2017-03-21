8f16e5b79af37e567a56582a28b2f824ebcfe2ff
Pedro Castanheira <josepedrocastanheira@gmail.com>
2017-03-21 15:42:42 +0000

    Refactor options.js
    Removed setTimeout using promises.

--- src/static/js/options/options.js
+++ src/static/js/options/options.js
@@ -1,6 +1,24 @@
+var resultsPromise = new Promise(function(resolve, reject){
+  chrome.storage.local.get(function(result){
+    resolve(result.stackOverflowResults);
+  });
+});
+
+function loadOptions(resultsPromise) {
+  resultsPromise.then(function(stackOverflowStoredResults){
+    var currentValue = stackOverflowStoredResults ? stackOverflowStoredResults : "5";
+    var stackOverflowResultsOption = document.querySelector('option[value="' + currentValue + '"]');
+    stackOverflowResultsOption.selected = "selected";
+  });
+}
+
 function saveOptions() {
   var stackOverflowResults = document.getElementById('stackOverflowResults').value;
   chrome.storage.local.set({"stackOverflowResults": stackOverflowResults});
+  messageConfirmation();
+}
+
+function messageConfirmation(){
   var messageConfirmation = document.getElementById('saveConfirmation');
   messageConfirmation.innerHTML = "Saved successfully!";
   setTimeout(function() {
@@ -8,21 +26,9 @@ function saveOptions() {
   }, 1000);
 }
 
-function loadOptions() {
-  var stackOverflowStoredResults;
-  chrome.storage.local.get(function(result){
-    stackOverflowStoredResults = result.stackOverflowResults;
-  });
-  setTimeout(function(){
-    if (stackOverflowStoredResults) {
-      var currentValue = stackOverflowStoredResults;
-    } else {
-      var currentValue = "5";
-    }
-    var stackOverflowResultsOption = document.querySelector('option[value="' + currentValue + '"]');
-    stackOverflowResultsOption.selected = "selected"
-  }, 500);
-}
-
-document.addEventListener('DOMContentLoaded', loadOptions);
+document.addEventListener('DOMContentLoaded', loadOptions(resultsPromise));
 document.getElementById('save').addEventListener('click', saveOptions);
+
+module.exports = {
+  messageConfirmation
+}
