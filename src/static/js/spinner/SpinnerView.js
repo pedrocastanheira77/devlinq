7be4855847b0c113a20310f0c114dcd9dfacbf36
Pedro Castanheira <josepedrocastanheira@gmail.com>
2017-03-22 14:52:47 +0000

    Refactor and structure spinner

---
+++ src/static/js/spinner/SpinnerView.js
@@ -0,0 +1,46 @@
+var OPTS = {
+              lines: 13 // The number of lines to draw
+            , length: 28 // The length of each line
+            , width: 14 // The line thickness
+            , radius: 42 // The radius of the inner circle
+            , scale: 0.2 // Scales overall size of the spinner
+            , corners: 1 // Corner roundness (0..1)
+            , color: '#34A853' // #rgb or #rrggbb or array of colors
+            , opacity: 0.25 // Opacity of the lines
+            , rotate: 0 // The rotation offset
+            , direction: 1 // 1: clockwise, -1: counterclockwise
+            , speed: 1 // Rounds per second
+            , trail: 60 // Afterglow percentage
+            , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+            , zIndex: 2e9 // The z-index (defaults to 2000000000)
+            , className: 'spinner' // The CSS class to assign to the spinner
+            , top: '10%' // Top position relative to parent
+            , left: '50%' // Left position relative to parent
+            , shadow: false // Whether to render a shadow
+            , hwaccel: false // Whether to use hardware acceleration
+            , position: 'absolute' // Element positioning
+          };
+var d = document
+var spinner = new Spinner(OPTS);
+var spinnerDiv = new SpinnerDiv(d)
+StartSpinner(spinner, spinnerDiv);
+
+function SpinnerDiv(doc){
+  var spinnerDiv = doc.createElement("div");
+  spinnerDiv.id = "spinner";
+  return spinnerDiv
+}
+
+function StartSpinner(spinner, spinnerDiv) {
+  spinner.spin();
+  spinnerDiv.appendChild(spinner.el);
+  setTimeout(function() {
+    var resultStatsDiv = document.getElementById("resultStats");
+    resultStatsDiv.parentNode.insertBefore(spinnerDiv, resultStatsDiv);
+  }, 1500);
+}
+
+module.exports = {
+  SpinnerDiv,
+  StartSpinner
+}
---
+++ src/static/js/spinner/unittests/SpinnerView.test.js
@@ -0,0 +1,26 @@
+// var chai = require('chai');
+// var expect = require('chai').expect;
+// var spies = require('chai-spies');
+// chai.use(spies);
+// var SpinnerDiv = require('../SpinnerView.js').SpinnerDiv;
+// var jsdom = require('jsdom').jsdom;
+// var ourDocument = jsdom('<html><body><div id=""></div></body></html>');
+//
+// describe('SpinnerDiv', function() {
+//   it('Creates a div element to best the spinner', function(done){
+//     // SpinnerDiv(ourDocument);
+//     setTimeout(function(){
+//       expect(ourDocument.getElementById("spinner")).to.exist;
+//       done();
+//     }, 1000);
+//
+//   });
+// });
+//
+// describe('StartSpinner', function() {
+//
+//   it('Sets message confirmation innerHTML', function(){
+//     startSpinner(spinner);
+//   });
+//
+// });
