ce0a4a772b9d420d28361a9e308c9ca92973f19e
Lauren Rosie <laurenrosiewatson@gmail.com>
2017-03-22 14:44:10 +0000

    Refactor devlinq and add necessary tests for refactor

--- src/static/js/Devlinq.js
+++ src/static/js/Devlinq.js
@@ -7,22 +7,25 @@ var stackbar = new StackOverflowBar();
 var savedNumberOfLinks;
 var stackOverflowDiv = require("./stackoverflowbar/StackOverflowBar.js").stackOverflowDiv;
 
-// getRequestedNumberOfLinks();
-
-// devlinqExtention();
 
 function devlinqExtention(theDocument, theChrome) {
   setTimeout(function() {
-    loadFont();
-    replaceLogo(theDocument, theChrome);
-    createSpinner(theDocument);
-    var currentDiv = theDocument.getElementById("appbar");
-    lang.languagesDiv(currentDiv);
-    stackbar.stackOverflowDiv(currentDiv, savedNumberOfLinks);
+    preload(theDocument, theChrome)
+    createLangAndStackDiv(theDocument)
   }, 3000);
 }
 
-// Preload
+function preload(theDocument, theChrome){
+  loadFont();
+  replaceLogo(theDocument, theChrome);
+  createSpinner(theDocument);
+}
+
+function createLangAndStackDiv(theDocument){
+  var currentDiv = theDocument.getElementById("appbar");
+  lang.languagesDiv(currentDiv, theDocument);
+  stackbar.stackOverflowDiv(currentDiv, savedNumberOfLinks);
+}
 
 function loadFont() {
   WebFont.load({
@@ -47,62 +50,7 @@ function createSpinner(theDocument) {
   }
 }
 
-// Stack Overflow Div
-
-// function getRequestedNumberOfLinks() {
-//   chrome.storage.local.get(function(result){
-//     savedNumberOfLinks = result.stackOverflowResults;
-//   });
-// }
-
-// function stackOverflowDiv(currentDiv, requestedNumberOfLinks) {
-//   var stackOverflowDiv = createStackOverflowDiv();
-//   currentDiv.parentNode.insertBefore(stackOverflowDiv, currentDiv);
-//   createStackOverflowTitle(stackOverflowDiv);
-//   if (!requestedNumberOfLinks) {
-//       requestedNumberOfLinks = 5;
-//     }
-//   insertStackOverflowAPI(requestedNumberOfLinks, stackOverflowDiv);
-// }
-//
-// function createStackOverflowTitle(stackOverflowDiv) {
-//   var stackOverflowTitle = document.createElement("h2");
-//   stackOverflowTitle.className = "stackoverflow_title";
-//   stackOverflowTitle.insertAdjacentHTML('afterbegin', "STACK OVERFLOW");
-//   stackOverflowDiv.insertAdjacentElement('afterbegin', stackOverflowTitle);
-//   return stackOverflowTitle;
-// }
-//
-// function createStackOverflowDiv() {
-//   var stackOverflowDiv = document.createElement("div");
-//   stackOverflowDiv.id = "stackoverflowbar";
-//   stackOverflowDiv.className = "devlinq_div stackoverflow_div";
-//   return stackOverflowDiv;
-// }
-//
-// function insertStackOverflowAPI(requestedNumberOfLinks, stackOverflowDiv){
-//   var stackoverflowsearch = stackbar.decideStringForAPI();
-//   stackbar.getStackAPI(stackoverflowsearch, requestedNumberOfLinks).then(function(items){
-//     var numberOfLinks = Math.min(requestedNumberOfLinks, items.length);
-//     var googleResultUrls = document.getElementsByClassName("_Rm");
-//     for(var i = 0; i < numberOfLinks; i++){
-//       stackOverflowDiv.insertAdjacentHTML('beforeend',
-//         '<div class="so_item"><a href='+items[i].getUrl()+'><p class="linq linq_so">'+items[i].getTitle()+'</p><p class="so_info">View Count: '+items[i].getViewCount()+'; Answer Count: '+items[i].getAnswerCount()+'; Score: '+items[i].getScore()+'</p></a></div>');
-//       for(var x = 0; x < googleResultUrls.length; x++){
-//         if (items[i].getUrl().includes(googleResultUrls[x].innerHTML)){
-//           var box = googleResultUrls[x].parentNode.parentNode.parentNode.parentNode;
-//           if (box) {box.parentNode.removeChild(box);}
-//         }
-//       }
-//     }
-//   });
-// }
-
 module.exports = {
-  // stackOverflowDiv,
-  // createStackOverflowTitle,
-  // createStackOverflowDiv,
-  // insertStackOverflowAPI,
   devlinqExtention,
   createSpinner,
   replaceLogo,
--- src/static/js/unittests/Devlinq.test.js
+++ src/static/js/unittests/Devlinq.test.js
@@ -5,18 +5,39 @@ chai.use(spies);
 var WebFont = require('webfontloader');
 var loadFont = require('../Devlinq.js').loadFont;
 var createSpinner = require('../Devlinq.js').createSpinner;
-
+var devlinqExtention = require('../Devlinq.js').devlinqExtention;
 var jsdom = require('jsdom');
-var ourDocument = jsdom.jsdom ('<a id="logo"><div><div id="hello"></div></div></a>');
+var ourDocument = jsdom.jsdom ('<div><a id="logo"><div><div id="appbar"><div></div></div></div></a></div>');
 var ourChrome = require('sinon-chrome');
 var replaceLogo = require('../Devlinq.js').replaceLogo;
 var stub = require('sinon').stub;
+var StackOverflowBar = require("../stackoverflowbar/StackOverflowBar.js");
+
 
 describe('Devlinq', function(){
+  describe('#devlinqExtention', function(){
+    it('calls languagesDiv', function(done){
+      var LanguagesView = require("../languagebar/LanguagesBar.js");
+      stub(LanguagesView.prototype, 'languagesDiv');
+      stub(StackOverflowBar.prototype, 'stackOverflowDiv');
+      var spy = chai.spy.on(LanguagesView.prototype, 'languagesDiv');
+      devlinqExtention(ourDocument, ourChrome);
+      setTimeout(function(){
+        expect(spy).to.have.been.called();
+        done();
+      }, 3000);
+    });
 
-  // describe('#devlinqExtention', function(){
-  //
-  // };
+    it('calls stackoverflowdiv', function(done){
+      var LanguagesView = require("../languagebar/LanguagesBar.js");
+      var spy = chai.spy.on(StackOverflowBar.prototype, 'stackOverflowDiv');
+      devlinqExtention(ourDocument, ourChrome);
+      setTimeout(function(){
+        expect(spy).to.have.been.called();
+        done();
+      }, 3000);
+    });
+  });
 
   describe('#loadFont', function(){
     it('calls Webfont.load with correct arguments', function(){
