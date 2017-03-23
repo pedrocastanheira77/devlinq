1a1aed5652e9c4ad98764e43ac597782d5aa1188
Ekaterina Loshchinina <KateLoschinina@Ekaterinas-MacBook-Pro-4.local>
2017-03-23 10:18:49 +0000

    Refactor React App.js.

--- src/static/js/App.js
+++ src/static/js/App.js
@@ -1,32 +1,28 @@
 import React, { Component } from 'react';
-import '../css/style.css';
 import '../css/App.css';
+import '../css/style.css';
 var StackOverflowBar = require('./stackoverflowbar/StackOverflowBar.js');
-var stackbar = new StackOverflowBar();
-var currentDiv = document.getElementById("appbar");
-var value = document.getElementById("lst-ib").value
-// var devlinqExtention = require('./Devlinq.js').devlinqExtention;
-// var loadFont = require('./Devlinq.js').loadFont;
-// var languagesDiv = require('./Devlinq.js').languagesDiv;
 var LanguagesView = require("./languagebar/LanguagesBar.js");
-var lang = new LanguagesView();
-// var stackOverflowDiv = require('./Devlinq.js').stackOverflowDiv;
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {items: []};
+    this.state = {items: [],
+      stackbar: new StackOverflowBar(),
+      currentDiv: document.getElementById("appbar"),
+      value: document.getElementById("lst-ib").value,
+      lang: new LanguagesView()};
   }
 
   componentDidMount() {
     var that = this;
-    stackbar.getStackAPI(value, 5).then(function(items) {
+    this.state.stackbar.getStackAPI(that.state.value, 5).then(function(items) {
       that.setState({items: items});
     })
 
     document.querySelector("#lst-ib").addEventListener('change', function(evt) {
-      lang.languagesDiv(currentDiv);
-      stackbar.getStackAPI(document.getElementById("lst-ib").value, 5).then(function(items) {
+      this.state.lang.languagesDiv(this.state.currentDiv);
+      this.state.stackbar.getStackAPI(document.getElementById("lst-ib").value, 5).then(function(items) {
         that.setState({items: items});
       })
     })
@@ -35,7 +31,7 @@ class App extends Component {
   render(data) {
     return (
       <div className="App">
-       <div id="language">{lang.languagesDiv(currentDiv)}</div>
+       <div id="language">{this.state.lang.languagesDiv(this.state.currentDiv)}</div>
        <div className="devlinq_div stackoverflow_div" id="stackoverflowbar">
         <h2 className="stackoverflow_title">STACK OVERFLOW</h2>
           {this.state.items.map((item, i) => {
@@ -43,10 +39,9 @@ class App extends Component {
               <div key={i} className="so_item">
                 <a href="{item.getUrl()}">
                   <p className="linq linq_so">{item.getTitle()}</p>
-                  <p className="so_info">View Count: <mark className="score">{item.getViewCount()}</mark>Answer Count: <mark className="score">{item.getAnswerCount()}</mark>Score: <mark className="score">{item.getScore()}</mark></p>
+                  <p className="so_info">View Count: {item.getViewCount()}; Answer Count: {item.getAnswerCount()}; Score: {item.getScore()}.</p>
                 </a>
-              </div>
-            )
+              </div>)
           })}
        </div>
       </div>
